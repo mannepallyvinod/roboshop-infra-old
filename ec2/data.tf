988af850832fdfd70effe4c8572f455d1c0f782e
vinodkumar <vinod199733@gmail.com>
2023-04-08 16:25:19 +0530

    Auto commit

--- ec2/data.tf
+++ ec2/data.tf
@@ -2,6 +2,6 @@ data "aws_caller_identity" "current" {}
 
 data "aws_ami" "ami" {
   most_recent = true
-  name_regex  = "devops-practice-with-ansible"
+  name_regex  = "Ansible-AMI"
   owners      = [data.aws_caller_identity.current.account_id]
 }
